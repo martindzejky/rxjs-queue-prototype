0b42552bc3c96bac144983e803c610bb9d4e77ee
Martin Jakubik <martin@jakubik.info>
2020-05-05 09:46:21 +0200

    implement the manual process function

--- src/queue.ts
+++ src/queue.ts
@@ -1,6 +1,6 @@
 import { Command } from './command';
-import { Subject, BehaviorSubject } from 'rxjs';
-import { scan } from 'rxjs/operators';
+import { Subject, BehaviorSubject, from } from 'rxjs';
+import { buffer, filter, flatMap, scan } from 'rxjs/operators';
 
 /**
  * The purpose of this prototype - the **Queue**! It handles commands,
@@ -11,9 +11,11 @@ export class Queue {
     constructor() {
         // create the subjects
         this.rawEnqueuedCommands = new Subject<Command>();
-        this.counter = new BehaviorSubject<number>(0);
+        this.counterSubject = new BehaviorSubject<number>(0);
+        this.processSubject = new Subject<void>();
 
         this.initializeCounter();
+        this.initializeQueueProcessing();
     }
 
     /**
@@ -26,14 +28,16 @@ export class Queue {
     /**
      * Forces the queue to process all commands.
      */
-    process(): void {}
+    process(): void {
+        this.processSubject.next();
+    }
 
     /**
      * Returns the length of the queue - i. e. the number of yet
      * unprocessed commands.
      */
     length(): number {
-        return this.counter.value;
+        return this.counterSubject.value;
     }
 
     // PRIVATE
@@ -44,13 +48,40 @@ export class Queue {
     private initializeCounter() {
         this.rawEnqueuedCommands
             .pipe(scan(acc => acc + 1, 0))
-            .subscribe(this.counter);
+            .subscribe(this.counterSubject);
+    }
+
+    /**
+     * Initialize the manual process pipeline.
+     */
+    private initializeQueueProcessing() {
+        this.rawEnqueuedCommands
+            .pipe(
+                buffer(this.processSubject),
+
+                // convert the buffered command array back into
+                // a sequence of emitted commands
+                flatMap(commands => from(commands)),
+
+                // filter only those that have a callback functions,
+                // other commands are already done
+                filter(command => !!command.callback),
+            )
+            .subscribe(command => {
+                command.callback!({});
+            });
     }
 
     /**
      * Counter subject for storing the current queue length.
      */
-    private counter: BehaviorSubject<number>;
+    private counterSubject: BehaviorSubject<number>;
+
+    /**
+     * This subject emits when the queue is manually triggered to process
+     * its commands.
+     */
+    private processSubject: Subject<void>;
 
     /**
      * Raw commands coming from outside. Commands are pushed imperatively
